70ce43117c8bb08c858eb2ef7029df915db288be
zjh308 <1127893332@qq.com>
2020-08-22 09:52:44 +0800

    修改主页的风格

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,74 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="header"></div>
+   <!-- <router-link to="/login">登录页</router-link>
+   <router-link to="/register">注册页</router-link> -->
+   
+     
+     <div class="logo">
+          <span class="iconfont iconnew">  
+      </span>
+     </div>
+     <div class="changeWeb">
+        <h4>已有账号?请登录! 如果没有赶紧去注册吧!</h4>
+    </div>
+    <div class="entrance">
+       <button class="login" @click="login">登录</button>
+      <button class="register" @click="register">注册</button>
+    </div> 
+   
   </div>
+ 
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+ 
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  methods: {
+    login(){
+      this.$router.push({name:'Login'})
+    },
+    register(){
+      this.$router.push({name:'Register'})
+    }
+  },
+ 
 }
 </script>
+<style lang="less" scoped>
+ .header{
+        background-color: #757575;
+        width: 100%;
+        height: 30/360*100vw;
+    }
+   
+    .logo{
+        text-align: center;
+        .iconnew{
+            font-size: 80/360*100vw;
+            color: #d81e06;
+        }
+    }
+    .changeWeb{
+      width: 90%;
+      margin: 20px auto;
+    }
+    .entrance{
+      width: 80%;
+      height: 100px;
+      margin: 30px auto;
+      
+       display: flex;
+       justify-content: space-between;
+      .login{
+        width: 40%;
+        border: none;
+      }
+      .register{
+        width: 40%;
+        border: none;
+      }
+    }
+</style>
