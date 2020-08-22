49efda921522147faa82e5eb954013b03091bc31
zjh308 <1127893332@qq.com>
2020-08-22 09:50:44 +0800

    引入vant组件以及axios组件

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+import Vant from 'vant'
+
+import 'vant/lib/index.css'
+
+import axios from "axios";
+
+Vue.prototype.$axios = axios;
+
+Vue.use(Vant)
+
 new Vue({
   router,
   render: function (h) { return h(App) }
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
+import Login from '@/views/Auth/Login'
+
+import Register from '@/views/Auth/Register'
+
+import Center from '@/views/Auth/Center'
+
 Vue.use(VueRouter)
 
   const routes = [
@@ -11,15 +17,21 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
+    path:'/login',
+    name:'Login',
+    component:Login
+  },
+  {
+    path:'/register',
+    name:'Register',
+    component:Register
+  },
+  {
+    path:'/center',
+    name:'Center',
+    component:Center
   }
+ 
 ]
 
 const router = new VueRouter({
