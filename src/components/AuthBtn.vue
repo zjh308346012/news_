57716607c7b960cd2eb73ec88d646f3a79f0f5e1
zjh308 <1127893332@qq.com>
2020-08-22 09:51:33 +0800

    封装按钮页面

---
+++ src/components/AuthBtn.vue
@@ -0,0 +1,35 @@
+<template>
+   <div class="btn" @click="loginBtn">
+              <button >{{btnText}}</button>
+        </div>
+</template>
+
+<script>
+export default {
+     props:['btnText'],
+     methods:{
+         loginBtn(){
+             this.$emit('clickedBtn')
+         }
+     }
+}
+</script>
+
+<style lang="less" scoped>
+ .btn{
+        border: none;
+        margin-top: 30/360*100vw;
+        text-align: center;
+        button{
+        overflow: hidden;
+        width: 80%;
+        line-height: 40/360*100vw;
+        color: #fff;
+        background-color: #cc3300;
+        border: none;
+        border-radius: 20/360*100vw;
+        outline: none;
+        
+       }
+    }
+</style>
