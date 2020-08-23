abc237f6fe6151cd43e8f28acf7b0aaabfd04f7a
zjh308 <1127893332@qq.com>
2020-08-23 08:43:33 +0800

    利用组件封装

---
+++ src/components/Authnew.vue
@@ -0,0 +1,14 @@
+<template>
+  <van-cell :title="labelText" is-link :value="descText" height='300px'/>
+</template>
+
+<script>
+
+export default {
+   props:['labelText','descText']
+}
+</script>
+
+<style>
+
+</style>
