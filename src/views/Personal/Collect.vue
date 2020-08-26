5ef0d4af522727941d31c04536147d0af3c76398
zjh308 <1127893332@qq.com>
2020-08-26 10:41:07 +0800

    添加收藏页

---
+++ src/views/Personal/Collect.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="collectWrapper">
+       <div class="header"></div>
+       <AuthHead headText='我的收藏' @goBackcenter='back'/>
+       </div>
+</template>
+
+<script>
+import AuthHead from '@/components/AuthHead'
+export default {
+   components:{
+       AuthHead
+   },
+   methods:{
+       back(){
+           this.$router.push({name:'Index'})
+       }
+   }
+}
+</script>
+
+<style lang="less" scoped>
+.collectWrapper{
+    .header{
+        background-color: #757575;
+        width: 100%;
+        height: 30/360*100vw;
+    }   
+}
+</style>
