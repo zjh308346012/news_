56646920f5b16e716bc336fa0600d16904e4b53a
zjh308 <1127893332@qq.com>
2020-08-26 10:40:31 +0800

    添加页面的路由

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,24 @@ import Personal from '@/views/Personal/Index'
 import Edit from '@/views/Personal/Edit'
 //编辑资料页
 
+import Focus from '@/views/Personal/Focus'
+
+//我的关注页
+
+import Follow from '@/views/Personal/Follow'
+
+//我的跟帖页
+
+import Collect from '@/views/Personal/Collect'
+
+//我的收藏页
+
+import Indexs from '@/views/News/Indexs'
+
+//新闻主页
+
+import Addfans from '@/views/Personal/Addfans'
+
 Vue.use(VueRouter)
 
   const routes = [
@@ -41,6 +59,31 @@ Vue.use(VueRouter)
      path:'/edit',
      name:'Edit',
      component:Edit
+   },
+   {
+     path:'/focus',
+     name:'Focus',
+     component:Focus
+   },
+   {
+     path:'/follow',
+     name:'Follow',
+     component:Follow
+   },
+   {
+     path:'/collect',
+     name:'Collect',
+     component:Collect
+   },
+   {
+     path:'/indexs',
+     name:'Indexs',
+     component:Indexs
+   },
+   {
+     path:'/addfans',
+     name:'Addfans',
+     component:Addfans
    }
 ]
 
