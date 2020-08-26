c42046058c01c87e77259cd6780a5ea793d7905c
zjh308 <1127893332@qq.com>
2020-08-26 10:40:56 +0800

    添加我的关注页

---
+++ src/views/Personal/Addfans.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button @click="addFollow(1)">添加关注用户1</button>
+    <button @click="addFollow(2)">添加关注用户2</button>
+    <button @click="addFollow(3)">添加关注用户3</button>
+    <button @click="addFollow(4)">添加关注用户4</button>
+    <button @click="addFollow(5)">添加关注用户5</button>
+    <button @click="addFollow(6)">添加关注用户6</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    addFollow(id) {
+      this.$axios({
+        url: "/user_follows/" + id,
+        method: "get",
+         headers: {
+
+        Authorization: "Bearer " + localStorage.getItem('token')
+          //Bearer是标准化
+    }
+      }).then(res => {
+        console.log(res.data);
+      });
+    }
+  }
+};
+</script>
+<style>
+
+</style>
