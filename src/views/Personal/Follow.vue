540719fc0b5b4bbf20e2c3f2db2d6791c40be886
zjh308 <1127893332@qq.com>
2020-08-26 10:41:53 +0800

    添加跟帖页

---
+++ src/views/Personal/Follow.vue
@@ -0,0 +1,30 @@
+<template>
+   <div class="followWrapper">
+       <div class="header"></div>
+       <AuthHead headText='我的跟帖' @goBackcenter='back'/>
+       </div>
+</template>
+
+<script>
+import AuthHead from '@/components/AuthHead'
+export default {
+   components:{
+       AuthHead
+   },
+   methods:{
+       back(){
+           this.$router.push({name:'Index'})
+       }
+   }
+}
+</script>
+
+<style lang="less" scoped>
+.followWrapper{
+    .header{
+        background-color: #757575;
+        width: 100%;
+        height: 30/360*100vw;
+    }   
+}
+</style>
